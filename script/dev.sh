fb00638088828dc4d7827e79d58775ff2b6251c0
Adheli Tavares <adheli.tavares@gmail.com>
2019-02-21 08:24:57 +0000

    Adding configurations for mint 19.1

--- script/dev.sh
+++ script/dev.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -eux
 
 # Updating and Upgrading dependencies
-apt-get update -y
-apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # Install some system packages
-apt-get -y install python3 python-pip python-setuptools python3-distutils git python-dev
+sudo apt-get -y install python3 python-pip python-setuptools python3-distutils git python-dev
 
 # Install gnupg
-pip install gnupg
+sudo pip install gnupg
 
 # Install enum34
-pip install enum34
+sudo pip install enum34
 
 # Install things for mocking thing
-pip install tox
-pip install pipenv
-pip install dill
+sudo pip install tox
+sudo pip install pipenv
+sudo pip install dill
 
 # Install pycharm
-apt-get -y install snapd
+sudo apt-get -y install snapd
 snap install pycharm-community --classic
 
 # External tools
@@ -28,7 +28,7 @@ sudo pip install bandit
 sudo pip install pylint
 
 # docker
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt-get update -y
@@ -36,8 +36,9 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
 # tsocks
-apt-get install tsocks
+sudo apt-get install tsocks
 echo "server = 127.0.0.1" >> ~/.tsocks
 echo "server_port = 10922" >> ~/.tsocks
-echo "export TSOCKS_CONF_FILE=/home/user/.tsocks" >> ~/.bashrc
+echo "export TSOCKS_CONF_FILE=/home/$USER/.tsocks" >> ~/.bashrc
+echo "export PATH=\"$PATH:$TSOCKS_CONF_FILE\"" >> ~/.bashrc
 source ~/.bashrc
--- tpl/vagrantfile-mint-cinnamon-19.tpl
+++ tpl/vagrantfile-mint-cinnamon-19.tpl
@@ -5,8 +5,8 @@ Vagrant.configure("2") do |config|
   config.vm.provider :virtualbox do |v, override|
     v.gui = true
     v.customize ["modifyvm", :id, "--memory", 8192]
-    v.customize ["modifyvm", :id, "--cpus", 4]
-    v.customize ["modifyvm", :id, "--vram", "512"]
+    v.customize ["modifyvm", :id, "--cpus", 2]
+    v.customize ["modifyvm", :id, "--vram", "256"]
     v.customize ["setextradata", "global", "GUI/MaxGuestResolution", "any"]
     v.customize ["setextradata", :id, "CustomVideoMode1", "1024x768x32"]
     v.customize ["modifyvm", :id, "--ioapic", "on"]
@@ -14,4 +14,7 @@ Vagrant.configure("2") do |config|
     v.customize ["modifyvm", :id, "--accelerate3d", "on"]
     v.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
   end
+
+  config.vm.provision "shell", inline: "sudo usermod -aG docker $USER"
+
 end
